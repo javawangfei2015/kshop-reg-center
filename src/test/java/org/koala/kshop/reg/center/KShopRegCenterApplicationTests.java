20a22b120c8b7286b78bd2208eb9347a7a14322c
koala <javawangfei@163.com>
2017-12-31 17:11:03 +0800

    init

---
+++ src/test/java/org/koala/kshop/reg/center/KShopRegCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package org.koala.kshop.reg.center;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KShopRegCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
